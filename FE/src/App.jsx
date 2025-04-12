ae0781e0d5459183c6e303d98740b2091ca59e2f
Angie C <a.cajusol98@gmail.com>
2025-04-12 22:37:29 +0200

    little css

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Thread from './pages/Thread';
 import Login from './pages/Login';
+import SignUp from "./pages/SignUp";
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/threads/:threadId" element={<Thread />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
